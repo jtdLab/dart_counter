a268a7597f569966127d20d3521490325a005600
Jonas Schlauch <mrjosch99@gmail.com>
2021-05-06 03:07:14 +0200

    md added

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -6,6 +6,7 @@ import 'package:dart_counter/injection.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/widgets.dart';
 import 'package:injectable/injectable.dart';
 
