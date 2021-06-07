da530a9c9dccfaa51312bf068a6e4957aebbbd4f
Jonas Schlauch <mrjosch99@gmail.com>
2021-06-07 18:44:05 +0200

    updated blocs

--- lib/presentation/ios/in_game/in_game_page.dart
+++ lib/presentation/ios/in_game/in_game_page.dart
@@ -8,6 +8,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:auto_route/auto_route.dart';
 
 class InGamePage extends StatelessWidget {
+
+  
+
   @override
   Widget build(BuildContext context) {
     return AppPage(
