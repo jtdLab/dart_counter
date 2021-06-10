ae27cae0aa010b07a25ce1fb25d476898c5b5341
Jonas Schlauch <mrjosch99@gmail.com>
2021-06-11 00:18:21 +0200

    small changes

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -3,9 +3,14 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:injectable/injectable.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'presentation/core/app_widget.dart';
 
 Future<void> main() async {
+  ResponsiveSizingConfig.instance.setCustomBreakpoints(
+    null,
+    customRefinedBreakpoints: const RefinedBreakpoints(mobileNormal: 360),
+  );
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   configureInjection(Environment.prod);
