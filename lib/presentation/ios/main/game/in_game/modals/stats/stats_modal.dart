974f13c362c5428c7501ce1f89143d559ea2c928
Jonas Schlauch <mrjosch99@gmail.com>
2021-10-13 18:50:35 +0200

    mock stats modal new

--- lib/presentation/ios/main/game/in_game/modals/stats/stats_modal.dart
+++ lib/presentation/ios/main/game/in_game/modals/stats/stats_modal.dart
@@ -1,8 +1,10 @@
 // CORE
+import 'package:dart_counter/domain/play/player_snapshot.dart';
 import 'package:dart_counter/presentation/ios/core/core.dart';
 
 // BLOCS
 import 'package:dart_counter/application/in_game/stats/stats_bloc.dart';
+import 'package:dart_counter/presentation/ios/main/shared.dart';
 
 // SHARED WIDGETS
 import './../../../shared.dart';
@@ -21,9 +23,9 @@ class StatsModal extends StatelessWidget {
       builder: (context, state) {
         return AppPage(
           padding: modalPagePadding(context),
-          child: const SingleChildScrollView(
+          //child: const SingleChildScrollView(
             child: _StatsWidget(),
-          ),
+          //),
         );
       },
     );
