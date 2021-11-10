50dc0f620df6ff22fab21c874697fa1258c39367
Jonas Schlauch <mrjosch99@gmail.com>
2021-11-10 22:48:07 +0100

    preperation of all infra test finished

--- lib/infrastructure/core/storage_helpers.dart
+++ lib/infrastructure/core/storage_helpers.dart
@@ -3,7 +3,11 @@ import 'package:dart_counter/domain/core/errors.dart';
 import 'package:dart_counter/injection.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+/// App specific extensions on firebase storage
 extension StorageX on FirebaseStorage {
+  /// Returns a reference to the profilePhoto storage of the app-user.
+  ///
+  /// Throws [NotAuthenticatedError] if app user is not signed in.
   Reference profilePhotoReference() {
     final uid = getIt<IAuthService>().userId();
 
