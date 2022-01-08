cb4c4e2775bdee46edec2b8c75c9a105ded8234d
Jonas Schlauch <mrjosch99@gmail.com>
2022-01-08 15:00:27 +0100

    finished impl in training first iteration

--- lib/presentation/ios/main/training/in_training/double/widgets.dart
+++ lib/presentation/ios/main/training/in_training/double/widgets.dart
@@ -358,8 +358,26 @@ class _TwoPlayerDisplayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.green,
+    return BlocBuilder<TrainingBloc, TrainingState>(
+      builder: (context, state) {
+        final players = state.gameSnapshot.players;
+
+        return AppRow(
+          spacing: size6(context),
+          children: [
+            Expanded(
+              child: PlayerItem(
+                player: players[0] as DoubleTrainingPlayerSnapshot,
+              ),
+            ),
+            Expanded(
+              child: PlayerItem(
+                player: players[1] as DoubleTrainingPlayerSnapshot,
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -372,8 +390,34 @@ class _ThreePlayerDisplayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.blue,
+    return BlocBuilder<TrainingBloc, TrainingState>(
+      builder: (context, state) {
+        final players = state.gameSnapshot.players;
+
+        return AppRow(
+          spacing: size6(context),
+          children: [
+            Expanded(
+              child: PlayerItem(
+                player: players[0] as DoubleTrainingPlayerSnapshot,
+              ),
+            ),
+            Expanded(
+              child: AppColumn(
+                spacing: size6(context),
+                children: [
+                  PlayerItemSmall(
+                    player: players[2] as DoubleTrainingPlayerSnapshot,
+                  ),
+                  PlayerItemSmall(
+                    player: players[1] as DoubleTrainingPlayerSnapshot,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -386,8 +430,44 @@ class _FourPlayerDisplayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.yellow,
+    return BlocBuilder<TrainingBloc, TrainingState>(
+      builder: (context, state) {
+        final players = state.gameSnapshot.players;
+
+        return AppColumn(
+          spacing: size6(context),
+          children: [
+            Expanded(
+              flex: 3,
+              child: AppRow(
+                spacing: size6(context),
+                children: [
+                  PlayerItemSmall(
+                    player: players[0] as DoubleTrainingPlayerSnapshot,
+                  ),
+                  PlayerItemSmall(
+                    player: players[1] as DoubleTrainingPlayerSnapshot,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: AppRow(
+                spacing: size6(context),
+                children: [
+                  PlayerItemSmall(
+                    player: players[2] as DoubleTrainingPlayerSnapshot,
+                  ),
+                  PlayerItemSmall(
+                    player: players[3] as DoubleTrainingPlayerSnapshot,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -481,6 +561,427 @@ class _KeyBoard extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+// TODO impl
+// SHARED
+// PLAYER ITEM
+class PlayerItem extends StatelessWidget {
+  // final ProfileImagePosition profileImagePosition; TODO
+  final DoubleTrainingPlayerSnapshot player;
+
+  const PlayerItem({
+    Key? key,
+    required this.player,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: border4(context),
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 7,
+            child: _PlayerItemHeader(
+              name: player
+                  .name!, // TODO DartBot has no name maybe do dartbot into seperate item
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: _PlayerItemTargetDisplayer(
+              target: player.targetValue,
+            ),
+          ),
+          Expanded(
+            flex: 30,
+            child: _PlayerItemPointsDisplayer(
+              points: 88, // TODO
+            ),
+          ),
+          // TODO introduce a super widghet to this and next widgets with them as a part ??
+          Expanded(
+            flex: 24,
+            child: _PlayerItemStatsDisplayer(
+              checkouts: 0, // TODO
+              dartsThrown: player.dartsThrown,
+              checkoutPercentage: player.checkoutPercentage,
+              missed: player.missed,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlayerItemHeader extends StatelessWidget {
+  final Color color;
+  final String? photoUrl;
+  final String name;
+
+  const _PlayerItemHeader({
+    Key? key,
+    this.color = AppColors.blueNew,
+    this.photoUrl,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          children: [
+            //const Spacer(),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: color,
+                child: Padding(
+                  padding: EdgeInsets.all(size6(context) / 4),
+                  child: Center(
+                    child: AutoSizeText(
+                      name,
+                      maxLines: 1,
+                      minFontSize: 4,
+                      style: CupertinoTheme.of(context)
+                          .textTheme
+                          .textStyle
+                          .copyWith(color: AppColors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        /**
+           * Align(
+            alignment: Alignment.centerLeft,
+            child: AppRoundedImage.small(
+              imageName: AppImages.photoPlaceholderNew, // TODO real image
+              border: Border.all(
+                width: border4(context) / 2,
+              ),
+            ),
+          ),
+           */
+      ],
+    );
+  }
+}
+
+class _PlayerItemTargetDisplayer extends StatelessWidget {
+  final int target;
+
+  const _PlayerItemTargetDisplayer({
+    Key? key,
+    required this.target,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.black,
+      child: Padding(
+        padding: EdgeInsets.all(size6(context) / 4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            AutoSizeText(
+              'ZIEL: D$target',
+              style: CupertinoTheme.of(context)
+                  .textTheme
+                  .textStyle
+                  .copyWith(color: AppColors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemPointsDisplayer extends StatelessWidget {
+  final int points;
+
+  const _PlayerItemPointsDisplayer({
+    Key? key,
+    required this.points,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: border4(context)),
+          bottom: BorderSide(width: border4(context)),
+        ),
+      ),
+      child: Center(
+        child: AutoSizeText(
+          points.toString(),
+          style: CupertinoTheme.of(context)
+              .textTheme
+              .textStyle
+              .copyWith(fontSize: 40), // TODO
+          maxLines: 1,
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemStatsDisplayer extends StatelessWidget {
+  final int checkouts;
+  final int dartsThrown;
+  final double? checkoutPercentage;
+  final int missed;
+
+  const _PlayerItemStatsDisplayer({
+    Key? key,
+    required this.checkouts,
+    required this.dartsThrown,
+    required this.checkoutPercentage,
+    required this.missed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: border4(context)),
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'CHECKOUTS',
+              value: checkouts.toString(),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'DARTS THROWN',
+              value: dartsThrown.toString(),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'CHECKOUT %',
+              value: checkoutPercentage.toString(),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'MISSED',
+              value: missed.toString(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlayerItemStatDisplayer extends StatelessWidget {
+  final String title;
+  final String? value;
+
+  const _PlayerItemStatDisplayer({
+    Key? key,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppRow(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      spacing: size6(context),
+      children: [
+        Text(title),
+        Text(
+          value ?? '-',
+        ),
+      ],
+    );
+  }
+}
+
+// PLAYER ITEM SMALL
+class PlayerItemSmall extends StatelessWidget {
+  final DoubleTrainingPlayerSnapshot player;
+
+  const PlayerItemSmall({
+    Key? key,
+    required this.player,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: border4(context),
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: _PlayerItemSmallHeader(
+                // TODO photorul
+                name: player.name!,
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: _PlayerItemSmallTargetDisplayer(
+                target: player.targetValue,
+              ),
+            ),
+            Expanded(
+              flex: 17,
+              child: _PlayerItemSmallPointsDisplayer(
+                points: 88, // TODO
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemSmallHeader extends StatelessWidget {
+  final Color color;
+  final String? photoUrl;
+  final String name;
+
+  const _PlayerItemSmallHeader({
+    Key? key,
+    this.color = AppColors.blueNew,
+    this.photoUrl,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          children: [
+            //const Spacer(),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: color,
+                child: Padding(
+                  padding: EdgeInsets.all(size6(context) / 4),
+                  child: Center(
+                    child: AutoSizeText(
+                      name,
+                      maxLines: 1,
+                      minFontSize: 4,
+                      style: CupertinoTheme.of(context)
+                          .textTheme
+                          .textStyle
+                          .copyWith(color: AppColors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        /**
+           * Align(
+            alignment: Alignment.centerLeft,
+            child: AppRoundedImage.small(
+              imageName: AppImages.photoPlaceholderNew, // TODO real image
+              border: Border.all(
+                width: border4(context) / 2,
+              ),
+            ),
+          ),
+           */
+      ],
+    );
+  }
+}
+
+class _PlayerItemSmallTargetDisplayer extends StatelessWidget {
+  final int target;
+
+  const _PlayerItemSmallTargetDisplayer({
+    Key? key,
+    required this.target,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.black,
+      child: Padding(
+        padding: EdgeInsets.all(size6(context) / 4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            AutoSizeText(
+              'ZIEL: D$target',
+              maxLines: 1,
+              minFontSize: 1,
+              style: CupertinoTheme.of(context)
+                  .textTheme
+                  .textStyle
+                  .copyWith(color: AppColors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemSmallPointsDisplayer extends StatelessWidget {
+  final int points;
+
+  const _PlayerItemSmallPointsDisplayer({
+    Key? key,
+    required this.points,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // TODO color
+      child: Center(
+        child: AutoSizeText(
+          points.toString(),
+          style: CupertinoTheme.of(context)
+              .textTheme
+              .textStyle
+              .copyWith(fontSize: 40), // TODO
+          maxLines: 1,
+        ),
+      ),
     );
   }
 }
--- lib/presentation/ios/main/training/in_training/single/widgets.dart
+++ lib/presentation/ios/main/training/in_training/single/widgets.dart
@@ -360,8 +360,26 @@ class _TwoPlayerDisplayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.green,
+    return BlocBuilder<TrainingBloc, TrainingState>(
+      builder: (context, state) {
+        final players = state.gameSnapshot.players;
+
+        return AppRow(
+          spacing: size6(context),
+          children: [
+            Expanded(
+              child: PlayerItem(
+                player: players[0] as SingleTrainingPlayerSnapshot,
+              ),
+            ),
+            Expanded(
+              child: PlayerItem(
+                player: players[1] as SingleTrainingPlayerSnapshot,
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -374,8 +392,34 @@ class _ThreePlayerDisplayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.blue,
+    return BlocBuilder<TrainingBloc, TrainingState>(
+      builder: (context, state) {
+        final players = state.gameSnapshot.players;
+
+        return AppRow(
+          spacing: size6(context),
+          children: [
+            Expanded(
+              child: PlayerItem(
+                player: players[0] as SingleTrainingPlayerSnapshot,
+              ),
+            ),
+            Expanded(
+              child: AppColumn(
+                spacing: size6(context),
+                children: [
+                  PlayerItemSmall(
+                    player: players[2] as SingleTrainingPlayerSnapshot,
+                  ),
+                  PlayerItemSmall(
+                    player: players[1] as SingleTrainingPlayerSnapshot,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -388,8 +432,44 @@ class _FourPlayerDisplayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.yellow,
+    return BlocBuilder<TrainingBloc, TrainingState>(
+      builder: (context, state) {
+        final players = state.gameSnapshot.players;
+
+        return AppColumn(
+          spacing: size6(context),
+          children: [
+            Expanded(
+              flex: 3,
+              child: AppRow(
+                spacing: size6(context),
+                children: [
+                  PlayerItemSmall(
+                    player: players[0] as SingleTrainingPlayerSnapshot,
+                  ),
+                  PlayerItemSmall(
+                    player: players[1] as SingleTrainingPlayerSnapshot,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: AppRow(
+                spacing: size6(context),
+                children: [
+                  PlayerItemSmall(
+                    player: players[2] as SingleTrainingPlayerSnapshot,
+                  ),
+                  PlayerItemSmall(
+                    player: players[3] as SingleTrainingPlayerSnapshot,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -500,6 +580,427 @@ class _KeyBoard extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+// TODO impl
+// SHARED
+// PLAYER ITEM
+class PlayerItem extends StatelessWidget {
+  // final ProfileImagePosition profileImagePosition; TODO
+  final SingleTrainingPlayerSnapshot player;
+
+  const PlayerItem({
+    Key? key,
+    required this.player,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: border4(context),
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 7,
+            child: _PlayerItemHeader(
+              name: player
+                  .name!, // TODO DartBot has no name maybe do dartbot into seperate item
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: _PlayerItemTargetDisplayer(
+              target: player.targetValue,
+            ),
+          ),
+          Expanded(
+            flex: 30,
+            child: _PlayerItemPointsDisplayer(
+              points: player.points,
+            ),
+          ),
+          // TODO introduce a super widghet to this and next widgets with them as a part ??
+          Expanded(
+            flex: 24,
+            child: _PlayerItemStatsDisplayer(
+              singles: player.singles,
+              doubles: player.doubles,
+              triples: player.triples,
+              missed: player.missed,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlayerItemHeader extends StatelessWidget {
+  final Color color;
+  final String? photoUrl;
+  final String name;
+
+  const _PlayerItemHeader({
+    Key? key,
+    this.color = AppColors.blueNew,
+    this.photoUrl,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          children: [
+            //const Spacer(),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: color,
+                child: Padding(
+                  padding: EdgeInsets.all(size6(context) / 4),
+                  child: Center(
+                    child: AutoSizeText(
+                      name,
+                      maxLines: 1,
+                      minFontSize: 4,
+                      style: CupertinoTheme.of(context)
+                          .textTheme
+                          .textStyle
+                          .copyWith(color: AppColors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        /**
+           * Align(
+            alignment: Alignment.centerLeft,
+            child: AppRoundedImage.small(
+              imageName: AppImages.photoPlaceholderNew, // TODO real image
+              border: Border.all(
+                width: border4(context) / 2,
+              ),
+            ),
+          ),
+           */
+      ],
+    );
+  }
+}
+
+class _PlayerItemTargetDisplayer extends StatelessWidget {
+  final int target;
+
+  const _PlayerItemTargetDisplayer({
+    Key? key,
+    required this.target,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.black,
+      child: Padding(
+        padding: EdgeInsets.all(size6(context) / 4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            AutoSizeText(
+              'ZIEL: D$target',
+              style: CupertinoTheme.of(context)
+                  .textTheme
+                  .textStyle
+                  .copyWith(color: AppColors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemPointsDisplayer extends StatelessWidget {
+  final int points;
+
+  const _PlayerItemPointsDisplayer({
+    Key? key,
+    required this.points,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: border4(context)),
+          bottom: BorderSide(width: border4(context)),
+        ),
+      ),
+      child: Center(
+        child: AutoSizeText(
+          points.toString(),
+          style: CupertinoTheme.of(context)
+              .textTheme
+              .textStyle
+              .copyWith(fontSize: 40), // TODO
+          maxLines: 1,
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemStatsDisplayer extends StatelessWidget {
+  final int singles;
+  final int doubles;
+  final int triples;
+  final int missed;
+
+  const _PlayerItemStatsDisplayer({
+    Key? key,
+    required this.singles,
+    required this.doubles,
+    required this.triples,
+    required this.missed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: border4(context)),
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'SINGLES',
+              value: singles.toString(),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'DOUBLES',
+              value: doubles.toString(),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'TRIPLES',
+              value: triples.toString(),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: _PlayerItemStatDisplayer(
+              title: 'MISSED',
+              value: missed.toString(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlayerItemStatDisplayer extends StatelessWidget {
+  final String title;
+  final String? value;
+
+  const _PlayerItemStatDisplayer({
+    Key? key,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppRow(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      spacing: size6(context),
+      children: [
+        Text(title),
+        Text(
+          value ?? '-',
+        ),
+      ],
+    );
+  }
+}
+
+// PLAYER ITEM SMALL
+class PlayerItemSmall extends StatelessWidget {
+  final SingleTrainingPlayerSnapshot player;
+
+  const PlayerItemSmall({
+    Key? key,
+    required this.player,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: border4(context),
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: _PlayerItemSmallHeader(
+                // TODO photorul
+                name: player.name!,
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: _PlayerItemSmallTargetDisplayer(
+                target: player.targetValue,
+              ),
+            ),
+            Expanded(
+              flex: 17,
+              child: _PlayerItemSmallPointsDisplayer(
+                points: player.points,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemSmallHeader extends StatelessWidget {
+  final Color color;
+  final String? photoUrl;
+  final String name;
+
+  const _PlayerItemSmallHeader({
+    Key? key,
+    this.color = AppColors.blueNew,
+    this.photoUrl,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          children: [
+            //const Spacer(),
+            Expanded(
+              flex: 5,
+              child: Container(
+                color: color,
+                child: Padding(
+                  padding: EdgeInsets.all(size6(context) / 4),
+                  child: Center(
+                    child: AutoSizeText(
+                      name,
+                      maxLines: 1,
+                      minFontSize: 4,
+                      style: CupertinoTheme.of(context)
+                          .textTheme
+                          .textStyle
+                          .copyWith(color: AppColors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        /**
+           * Align(
+            alignment: Alignment.centerLeft,
+            child: AppRoundedImage.small(
+              imageName: AppImages.photoPlaceholderNew, // TODO real image
+              border: Border.all(
+                width: border4(context) / 2,
+              ),
+            ),
+          ),
+           */
+      ],
+    );
+  }
+}
+
+class _PlayerItemSmallTargetDisplayer extends StatelessWidget {
+  final int target;
+
+  const _PlayerItemSmallTargetDisplayer({
+    Key? key,
+    required this.target,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.black,
+      child: Padding(
+        padding: EdgeInsets.all(size6(context) / 4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            AutoSizeText(
+              'ZIEL: D$target',
+              maxLines: 1,
+              minFontSize: 1,
+              style: CupertinoTheme.of(context)
+                  .textTheme
+                  .textStyle
+                  .copyWith(color: AppColors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PlayerItemSmallPointsDisplayer extends StatelessWidget {
+  final int points;
+
+  const _PlayerItemSmallPointsDisplayer({
+    Key? key,
+    required this.points,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // TODO color
+      child: Center(
+        child: AutoSizeText(
+          points.toString(),
+          style: CupertinoTheme.of(context)
+              .textTheme
+              .textStyle
+              .copyWith(fontSize: 40), // TODO
+          maxLines: 1,
+        ),
+      ),
     );
   }
 }
