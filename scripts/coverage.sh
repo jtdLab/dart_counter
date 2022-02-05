276602a0bcd2634c23e62c473108575c37c9618e
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-02-05 16:06:45 +0100

    update coverage script to not gen temporary file anymore

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,6 +4,6 @@ cd ..
 # Run tests with coverage
 flutter test --coverage
 # Remove generated files
-lcov --remove coverage/lcov.info 'lib/generated/*' 'lib/injection.config.dart' 'lib/presentation/ios/router.gr.dart' 'lib/presentation/android/router.gr.dart' -o coverage/new_lcov.info
+lcov --remove coverage/lcov.info 'lib/generated/*' 'lib/injection.config.dart' 'lib/presentation/ios/router.gr.dart' 'lib/presentation/android/router.gr.dart' -o coverage/lcov.info
 # Generate coverage report
-genhtml coverage/new_lcov.info -o coverage/output/
+genhtml coverage/lcov.info -o coverage/output/
