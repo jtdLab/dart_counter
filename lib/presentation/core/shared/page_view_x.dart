45d4b197809654b938068b2e85ac6fc49353111f
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-03-25 23:52:15 +0100

    add future todo

---
+++ lib/presentation/core/shared/page_view_x.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+
+// TODO some how make page view able to get its page change logged
+
+/**
+ * extension PageViewX on PageView {
+  void didNavigate(String oldPageName, String newPageName) {
+    var currentPage = 0;
+
+    final newPage = controller.page;
+
+    if (newPage == 0 || currentPage == 1) {
+      currentPage = newPage!.toInt();
+    }
+  }
+}
+
+ */
