91b4f6a0276eaae9045210edee912f738760f661
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-03-30 15:32:35 +0200

    init tests

--- lib/presentation/ios/unauthenticated/auth/sign_in/sign_in_page.dart
+++ lib/presentation/ios/unauthenticated/auth/sign_in/sign_in_page.dart
@@ -6,40 +6,45 @@ import 'package:dart_counter/presentation/ios/core/core.dart';
 
 part 'widgets.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatelessWidget implements AutoRouteWrapper {
   const SignInPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget wrappedRoute(BuildContext context) {
     return BlocProvider(
       create: (context) => SignInBloc.getIt(),
-      child: MultiBlocListener(
-        listeners: [
-          BlocListener<AuthBloc, AuthState>(
-            listener: (context, state) => state.whenOrNull(
-              authenticated: () =>
-                  context.router.replace(const AuthenticatedFlowRoute()),
-            ),
+      child: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<AuthBloc, AuthState>(
+          listener: (context, state) => state.whenOrNull(
+            authenticated: () =>
+                context.router.replace(const AuthenticatedFlowRoute()),
           ),
-          BlocListener<SignInBloc, SignInState>(
-            listenWhen: (_, next) => next is SignInLoadFailure,
-            listener: (context, state) => state.mapOrNull(
-              loadFailure: (signInLoadFailure) {
-                signInLoadFailure.failure.whenOrNull(
-                  // TODO show server error feels not perfect
-                  serverError: () => showToast(context.l10n.errorServer),
-                  invalidEmailAndPasswordCombination: () => showToast(
-                    context.l10n.errorInvalidEmailAndPasswordCombination,
-                  ),
-                );
-              },
-            ),
+        ),
+        BlocListener<SignInBloc, SignInState>(
+          listenWhen: (_, next) => next is SignInLoadFailure,
+          listener: (context, state) => state.mapOrNull(
+            loadFailure: (signInLoadFailure) {
+              signInLoadFailure.failure.whenOrNull(
+                // TODO show server error feels not perfect
+                serverError: () => showToast(context.l10n.errorServer),
+                invalidEmailAndPasswordCombination: () => showToast(
+                  context.l10n.errorInvalidEmailAndPasswordCombination,
+                ),
+              );
+            },
           ),
-        ],
-        child: const SignInView(),
-      ),
+        ),
+      ],
+      child: const SignInView(),
     );
   }
 }
--- lib/presentation/ios/unauthenticated/auth/sign_up/sign_up_page.dart
+++ lib/presentation/ios/unauthenticated/auth/sign_up/sign_up_page.dart
@@ -6,40 +6,45 @@ import 'package:dart_counter/presentation/ios/core/core.dart';
 
 part 'widgets.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatelessWidget implements AutoRouteWrapper {
   const SignUpPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget wrappedRoute(BuildContext context) {
     return BlocProvider(
       create: (context) => SignUpBloc.getIt(),
-      child: MultiBlocListener(
-        listeners: [
-          BlocListener<AuthBloc, AuthState>(
-            listener: (context, state) {
-              state.whenOrNull(
-                authenticated: () =>
-                    context.router.replace(const AuthenticatedFlowRoute()),
-              );
-            },
-          ),
-          BlocListener<SignUpBloc, SignUpState>(
-            listenWhen: (_, next) => next is SignUpLoadFailure,
-            listener: (context, state) {
-              state.mapOrNull(
-                loadFailure: (signInLoadFailure) {
-                  signInLoadFailure.failure.whenOrNull(
-                    serverError: () => showToast(context.l10n.errorServer),
-                  );
-                },
-              );
-            },
-          ),
-        ],
-        child: const SignUpView(),
-      ),
+      child: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<AuthBloc, AuthState>(
+          listener: (context, state) {
+            state.whenOrNull(
+              authenticated: () =>
+                  context.router.replace(const AuthenticatedFlowRoute()),
+            );
+          },
+        ),
+        BlocListener<SignUpBloc, SignUpState>(
+          listenWhen: (_, next) => next is SignUpLoadFailure,
+          listener: (context, state) {
+            state.mapOrNull(
+              loadFailure: (signInLoadFailure) {
+                signInLoadFailure.failure.whenOrNull(
+                  serverError: () => showToast(context.l10n.errorServer),
+                );
+              },
+            );
+          },
+        ),
+      ],
+      child: const SignUpView(),
     );
   }
 }
