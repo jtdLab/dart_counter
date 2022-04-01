165c54e52b855b6130a141c89172745fc84a600e
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-04-01 21:18:59 +0200

    test platform app widgets

--- test/presentation/ios/app_test.dart
+++ test/presentation/ios/app_test.dart
@@ -1,3 +1,45 @@
+import 'package:dart_counter/application/shared/auth/auth_bloc.dart';
+import 'package:dart_counter/presentation/ios/app.dart';
+import 'package:dart_counter/presentation/ios/core/core.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../core/helpers/helpers.dart';
+
 void main() {
-  // TODO implement tests
+  group('#App#', () {
+    late Router router;
+    late AuthBloc authBloc;
+
+    late App underTest;
+
+    setUp(() {
+      // Init dependencies
+      router = Router();
+      authBloc = MockAuthBloc();
+      whenListenTo(authBloc, const AuthState.unauthenticated());
+
+      // Init widget under test
+      underTest = const App();
+    });
+
+    Widget bootstrap() {
+      return MultiProvider(
+        providers: [
+          ListenableProvider.value(value: router),
+          BlocProvider.value(value: authBloc),
+        ],
+        child: underTest,
+      );
+    }
+
+    // TODO test that cupertino app is has correct params
+
+    testWidgets('Render CupertinoApp.', (tester) async {
+      // Act
+      await tester.pumpWidget(bootstrap());
+
+      // Assert
+      expect(find.byType(CupertinoApp), findsOneWidget);
+    });
+  });
 }
