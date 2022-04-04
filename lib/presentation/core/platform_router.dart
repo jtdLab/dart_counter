445052d2249570bbd34a278fcfe696cc1ad9ae2a
jtdLab <72231111+jtdLab@users.noreply.github.com>
2022-04-04 16:20:02 +0200

    lcov

--- lib/presentation/core/platform_router.dart
+++ lib/presentation/core/platform_router.dart
@@ -1,9 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/widgets.dart';
 
 // TODO doc
-abstract class PlatformRouter extends StackRouter {
-  PlatformRouter({
-    required LocalKey key,
-  }) : super(key: key);
-}
+abstract class PlatformRouter extends RootStackRouter {}
